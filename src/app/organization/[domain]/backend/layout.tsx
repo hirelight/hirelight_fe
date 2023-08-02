144900ce8bc510adef3e7fced74052ab833d0208
Nguyen Kien <ngkien299@gmail.com>
2023-08-02 23:47:15 +0700

    Create reuse layout for organization backend page

--- src/app/organization/[domain]/backend/layout.tsx
+++ src/app/organization/[domain]/backend/layout.tsx
@@ -8,7 +8,7 @@ export const metadata: Metadata = {
 
 const BackendLayout = ({ children }: { children: ReactNode }) => {
   return (
-    <div className='w-full min-h-screen'>
+    <div className='w-full min-h-screen flex flex-col pt-16'>
       <HeaderBar />
       {children}
     </div>
