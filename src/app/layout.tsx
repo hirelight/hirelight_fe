5c4a071b0ab2558c29a36fa9928a3af1e1f5aa75
Nguyen Kien <ngkien299@gmail.com>
2023-08-06 15:01:41 +0700

    Add Public_Sans fonts && colors to tailwind

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,23 @@
 import './globals.css';
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
+import { Inter, Public_Sans, Roboto_Mono } from 'next/font/google';
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-inter',
+});
+
+const roboto_mono = Roboto_Mono({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-roboto-mono',
+});
+const publicSans = Public_Sans({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-public-sans',
+});
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -16,7 +31,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en'>
-      <body className={inter.className}>{children}</body>
+      <body
+        className={`${inter.className} ${roboto_mono.className} ${publicSans.className}`}
+      >
+        {children}
+      </body>
     </html>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,14 @@ module.exports = {
     './src/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue_primary_800: '#0048B3',
+        blue_primary_600: '#097AFF',
+        neutral_900: '#343A4E',
+        neutral_700: '#5B607C',
+      },
+    },
   },
   plugins: [require('flowbite/plugin')],
 };
