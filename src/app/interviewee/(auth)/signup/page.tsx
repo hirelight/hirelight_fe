1d2bb3d32e6f501900af3acfb9d2bf2ce662f7b8
Nguyen Kien <ngkien299@gmail.com>
2023-08-09 16:03:59 +0700

    Create signup page for interviewee

---
+++ src/app/interviewee/(auth)/signup/page.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+
+import background from '/public/images/interviewee_auth_bg.png';
+import Image from 'next/image';
+import SignupForm from './components/SignupForm';
+
+import styles from './signup.module.scss';
+import Link from 'next/link';
+
+const IntervieweeSignup = () => {
+  return (
+    <div className='flex-1 flex relative'>
+      <div className='w-full lg:w-[45%] xl:flex-1 absolute inset-0 lg:relative opacity-20 lg:opacity-80'>
+        <Image
+          alt='Background'
+          src={background}
+          className='w-auto h-full object-cover'
+        />
+      </div>
+
+      <div className='flex-1 lg:bg-white relative'>
+        <div className='flex min-h-full flex-col px-6 pt-10 lg:px-8'>
+          <div className='sm:mx-auto sm:w-full sm:max-w-sm'>
+            <h2 className={styles.title__gradient}>Try Hirelight for free</h2>
+          </div>
+
+          <div className='mt-10 sm:mx-auto sm:w-full sm:max-w-xl'>
+            <SignupForm />
+            <p className='mt-10 text-center text-sm text-gray-500'>
+              Already have a Hirelight account?{' '}
+              <Link
+                href='/login'
+                className='font-semibold leading-6 text-indigo-600 hover:text-indigo-500'
+              >
+                Sign in
+              </Link>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default IntervieweeSignup;
