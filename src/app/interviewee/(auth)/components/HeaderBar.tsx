e8d4af5a27e8344cbc09d949adc48829d5c5a674
Nguyen Kien <ngkien299@gmail.com>
2023-08-09 16:03:25 +0700

    Create layout and HeaderBar for login, signup for interviewee

---
+++ src/app/interviewee/(auth)/components/HeaderBar.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HeaderBar = () => {
+  return (
+    <div className='w-full bg-blue_primary_600 flex justify-center items-center py-4 px-16'>
+      <div className='text-3xl font-medium text-white'>Hirelight</div>
+    </div>
+  );
+};
+
+export default HeaderBar;
---
+++ src/app/interviewee/(auth)/layout.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import HeaderBar from './components/HeaderBar';
+
+const IntervieweeAuthLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className='w-full h-screen flex flex-col md:overflow-hidden'>
+      <HeaderBar />
+      {children}
+    </div>
+  );
+};
+
+export default IntervieweeAuthLayout;
