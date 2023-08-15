52f589be237ba8d01f51136b3c3dd0e300c71af8
Nguyen Kien <ngkien299@gmail.com>
2023-08-15 18:29:17 +0700

    Create reuse Button component

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,27 @@
+import React, { ButtonHTMLAttributes, DetailedHTMLProps } from 'react';
+
+import styles from './Button.module.scss';
+
+interface IButton
+  extends DetailedHTMLProps<
+    ButtonHTMLAttributes<HTMLButtonElement>,
+    HTMLButtonElement
+  > {
+  isLoading?: boolean;
+}
+
+const Button = (props: IButton) => {
+  return (
+    <button
+      {...props}
+      className={
+        'text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-md text-sm w-full px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800' +
+        `${props.className} `
+      }
+    >
+      {props.children}
+    </button>
+  );
+};
+
+export default Button;
