0e164d9d2e3098d5e45c58c0e6ba6cd57ef0da80
Nguyen Kien <ngkien299@gmail.com>
2023-08-15 18:28:16 +0700

    Add routing to interviewee settings

---
+++ src/app/interviewee/settings/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const IntervieweeSettings = () => {
+  return (
+    <main className='w-full max-w-screen-xl mx-auto px-4 md:px-10'>
+      IntervieweeSettings
+    </main>
+  );
+};
+
+export default IntervieweeSettings;
