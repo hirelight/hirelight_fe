346f4fb6bd92126c5e45fd4ad891e831d6f05322
Nguyen Kien <ngkien299@gmail.com>
2023-09-07 21:54:13 +0700

    Fix title doesn't exist on NewJobHeader component

--- src/app/organization/[domain]/backend/jobs/[jobId]/layout.tsx
+++ src/app/organization/[domain]/backend/jobs/[jobId]/layout.tsx
@@ -8,7 +8,7 @@ const HiringPipelineDetailLayout = ({
 }) => {
   return (
     <div className='flex-1 flex flex-col max-w-screen-xl mx-auto'>
-      <NewJobHeader title={'title'} />
+      <NewJobHeader />
       <div className='flex-1 flex bg-slate-100 '>
         <div className='flex-1 max-w-screen-xl mx-auto pb-20'>{children}</div>
       </div>
