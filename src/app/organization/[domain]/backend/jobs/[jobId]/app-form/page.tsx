fbf1106585229a6ca7b15950703929005d4c876e
Nguyen Kien <ngkien299@gmail.com>
2023-09-12 23:19:56 +0700

    Initial AppForm component

--- src/app/organization/[domain]/backend/jobs/[jobId]/app-form/page.tsx
+++ src/app/organization/[domain]/backend/jobs/[jobId]/app-form/page.tsx
@@ -1,7 +1,20 @@
+import { Button, ButtonOutline } from '@/components';
 import React from 'react';
+import styles from './app-form.module.scss';
+import AppForm from './components/AppForm';
 
 const JobApplicationForm = () => {
-  return <div>JobApplicationForm</div>;
+  return (
+    <main>
+      <div className='w-full px-4 xl:px-0 flex justify-between items-center mb-4'>
+        <h3 className='text-sm uppercase font-bold text-neutral-700'>
+          Customize your application form
+        </h3>
+        <ButtonOutline className='ml-auto'>View destop mode</ButtonOutline>
+      </div>
+      <AppForm />
+    </main>
+  );
 };
 
 export default JobApplicationForm;
--- src/app/organization/[domain]/backend/jobs/[jobId]/layout.tsx
+++ src/app/organization/[domain]/backend/jobs/[jobId]/layout.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
 import NewJobHeader from '../components/NewJobHeader';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Hirelight - Backend',
+};
 
 const HiringPipelineDetailLayout = ({
   children,
@@ -9,7 +14,7 @@ const HiringPipelineDetailLayout = ({
   return (
     <div className='flex-1 flex flex-col max-w-screen-xl mx-auto'>
       <NewJobHeader />
-      <div className='flex-1 flex bg-slate-100 '>
+      <div className='flex-1 flex'>
         <div className='flex-1 max-w-screen-xl mx-auto pb-20'>{children}</div>
       </div>
     </div>
