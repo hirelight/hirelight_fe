aadfa95c2d8643e2da2375ac7fd9415bc7f38c6b
Nguyen Kien <ngkien299@gmail.com>
2023-09-13 16:52:05 +0700

    Create custom input & textarea components

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,7 @@
 import Button from './Button';
 import Selection from './Selection';
 import ButtonOutline from './ButtonOutline';
+import CustomInput from './CustomInput';
+import CustomTextArea from './CustomTextArea';
 
-export { Button, Selection, ButtonOutline };
+export { Button, Selection, ButtonOutline, CustomInput, CustomTextArea };
