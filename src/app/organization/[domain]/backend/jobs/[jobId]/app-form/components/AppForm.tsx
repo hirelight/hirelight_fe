a86b451fc707b15ce8eae9584cb8349a6ba96a49
Nguyen Kien <ngkien299@gmail.com>
2023-09-13 16:53:41 +0700

    App footer

--- src/app/organization/[domain]/backend/jobs/[jobId]/app-form/components/AppForm.tsx
+++ src/app/organization/[domain]/backend/jobs/[jobId]/app-form/components/AppForm.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import AppFormConfiguration from './AppFormConfiguration/AppFormConfiguration';
 import AppFormMobileView from './AppFormMobileView/AppFormMobileView';
+import { ButtonOutline } from '@/components';
 
 const AppForm = () => {
   return (
-    <form className='drop-shadow-lg'>
+    <form className='drop-shadow-lg relative'>
       <div className='flex relative'>
         <AppFormConfiguration />
         <AppFormMobileView />
       </div>
-      <div className='w-full bg-white px-3 py-4 border-t border-gray-300 rounded-bl-lg rounded-br-lg'></div>
+      <div className='w-full bg-white px-3 py-6 border-t border-gray-300 rounded-bl-lg rounded-br-lg relative z-[2]'>
+        <ButtonOutline>Save draft</ButtonOutline>
+      </div>
     </form>
   );
 };
