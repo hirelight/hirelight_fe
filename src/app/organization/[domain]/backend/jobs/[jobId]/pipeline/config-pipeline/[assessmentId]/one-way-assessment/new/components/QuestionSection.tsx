1b33027f15e7877f7964ad0a2794cf7b23b41302
Nguyen Kien <ngkien299@gmail.com>
2023-09-21 14:43:23 +0700

    Initial Ui for create new one-way assessment

---
+++ src/app/organization/[domain]/backend/jobs/[jobId]/pipeline/config-pipeline/[assessmentId]/one-way-assessment/new/components/QuestionSection.tsx
@@ -0,0 +1,106 @@
+import {
+    ArrowDownIcon,
+    ArrowUpIcon,
+    PencilIcon,
+    QuestionMarkCircleIcon,
+    TrashIcon,
+} from "@heroicons/react/24/solid";
+import React from "react";
+
+import AddNewQuestionSection from "../../components/AddNewQuestionSection";
+
+interface IQuestionSection {
+    data: {
+        id: number;
+        topic: string;
+        questions: {
+            id: number;
+            description: string;
+            thinkLength: string;
+            answerLength: string;
+            numOfTakes: number;
+        }[];
+    };
+    onUpdate: (updatedSection: any) => void;
+}
+
+const QuestionSection = ({ data, onUpdate }: IQuestionSection) => {
+    const [showEdit, setShowEdit] = React.useState(false);
+
+    if (showEdit) {
+        return (
+            <AddNewQuestionSection
+                data={data}
+                onSaveTopic={(updatedSection: any) => onUpdate(updatedSection)}
+                onFinish={() => {
+                    setShowEdit(false);
+                }}
+            />
+        );
+    }
+    return (
+        <div className="border border-slate-200 rounded-md">
+            <div className="flex justify-between items-center py-6 px-4 border-b border-slate-200">
+                <h4 className="text-lg text-neutral-700 flex items-center gap-1">
+                    {data.topic}{" "}
+                    <div className="inline-flex items-center gap-1 text-neutral-500">
+                        <span className="">
+                            <QuestionMarkCircleIcon className="w-6 h-6" />
+                        </span>
+                        <span className="text-sm">
+                            {data.questions.length} questions
+                        </span>
+                    </div>
+                </h4>
+                <div className="inline-flex items-center gap-4 text-neutral-500">
+                    <button type="button">
+                        <ArrowUpIcon className="w-6 h-6" />
+                    </button>
+                    <button type="button">
+                        <ArrowDownIcon className="w-6 h-6" />
+                    </button>
+                    <button
+                        type="button"
+                        className="group"
+                        onClick={() => setShowEdit(true)}
+                    >
+                        <PencilIcon className="w-6 h-6 group-hover:text-neutral-700" />
+                    </button>
+                    <button type="button" className="group">
+                        <TrashIcon className="w-6 h-6 text-red-400 group-hover:text-red-600" />
+                    </button>
+                </div>
+            </div>
+            <ul>
+                {data.questions.map((question, index) => (
+                    <li
+                        key={question.id}
+                        className="border-b last:border-b-0 border-slate-200 text-neutral-700 flex items-stretch"
+                    >
+                        <div className="w-16 aspect-square border-r border-slate-200 flex items-center justify-center">
+                            <span>{index + 1}</span>
+                        </div>
+                        <div className="flex-1 flex items-center justify-between gap-8 p-4">
+                            <p className="inline-block w-3/5">
+                                {question.description}
+                            </p>
+                            <div className="flex-1 flex-shrink-0 flex items-center justify-end gap-2 w-fit ml-auto text-sm uppercase font-semibold whitespace-nowrap">
+                                <span className="py-1 px-2.5 rounded-full bg-green-200 text-green-700">
+                                    {question.thinkLength}
+                                </span>
+                                <span className="py-1 px-2.5 rounded-full bg-blue-200 text-blue-700">
+                                    {question.answerLength} mins to answer
+                                </span>
+                                <span className="py-1 px-2.5 rounded-full bg-gray-200 text-gray-700">
+                                    {question.numOfTakes} takes
+                                </span>
+                            </div>
+                        </div>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export default QuestionSection;
--- src/app/organization/[domain]/backend/jobs/[jobId]/pipeline/config-pipeline/[assessmentId]/one-way-assessment/new/page.tsx
+++ src/app/organization/[domain]/backend/jobs/[jobId]/pipeline/config-pipeline/[assessmentId]/one-way-assessment/new/page.tsx
@@ -1,7 +1,127 @@
+"use client";
+
 import React from "react";
+import dynamic from "next/dynamic";
+import { PlusCircleIcon } from "@heroicons/react/24/solid";
+
+import { ButtonOutline, CustomInput, Portal, Selection } from "@/components";
+import { oneWayQuestions } from "@/utils/shared/initialDatas";
+
+import AddNewQuestionSection from "../components/AddNewQuestionSection";
+
+import QuestionSection from "./components/QuestionSection";
+
+const QuillEditorNoSSR = dynamic(() => import("@/components/QuillEditor"), {
+    ssr: false,
+    loading: () => <p>Loading ...</p>,
+});
 
 const NewOneWayInterview = () => {
-    return <div>NewOneWayInterview</div>;
+    const [showCreate, setShowCreate] = React.useState(false);
+    const [questionSections, setQuestionSections] =
+        React.useState(oneWayQuestions);
+
+    return (
+        <div className="bg-white py-4 shadow-md rounded-md">
+            <h2 className="text-xl text-neutral-700 font-medium px-4 xl:px-6 mb-8">
+                Create your video interview
+            </h2>
+
+            <h3 className="text-lg text-neutral-700 font-semibold bg-slate-100 p-4 xl:px-6 mb-6">
+                Welcome page info
+            </h3>
+
+            <div className="flex gap-6 mb-6 px-4 xl:px-6">
+                <CustomInput
+                    title="Title"
+                    type="text"
+                    placeholder="One-way interview - UX researcher at 123"
+                    required
+                />
+
+                <div className="w-1/3">
+                    <Selection
+                        title="Due date"
+                        datas={["In 7 days", "In 1 month", "In 3 days"]}
+                        onChange={() => {}}
+                        required
+                    />
+                </div>
+            </div>
+
+            <div className="min-h-[320px] flex flex-col gap-4 mb-6 px-4 xl:px-6">
+                <h3 className="text-neutral-700 font-medium">Welcome note</h3>
+                <QuillEditorNoSSR
+                    placeholder="Enter the job description here; include key areas of
+                    responsibility and what the candidate might do on a typical
+                    day."
+                    onChange={(value: string) => {}}
+                    className="flex-1 border border-slate-400 rounded-md overflow-hidden"
+                    theme="snow"
+                />
+            </div>
+
+            <section>
+                <h3 className="text-neutral-700 font-medium mb-4 p-4 xl:px-6 bg-slate-100">
+                    Questions
+                </h3>
+
+                <div className="px-4 xl:px-6 mb-6">
+                    <ul className="flex flex-col gap-6">
+                        {questionSections?.map(section => (
+                            <li key={section.id}>
+                                <QuestionSection
+                                    data={section}
+                                    onUpdate={updatedSection =>
+                                        setQuestionSections(prev =>
+                                            prev.map(item => {
+                                                if (
+                                                    item.id ===
+                                                    updatedSection.id
+                                                ) {
+                                                    return updatedSection;
+                                                }
+                                                return item;
+                                            })
+                                        )
+                                    }
+                                />
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+
+                <div className="mb-4 px-4 xl:px-6">
+                    {showCreate ? (
+                        <AddNewQuestionSection
+                            onFinish={() => setShowCreate(false)}
+                            onSaveTopic={(section: any) =>
+                                setQuestionSections(prev =>
+                                    prev.concat([
+                                        {
+                                            ...section,
+                                            id: questionSections.length,
+                                        },
+                                    ])
+                                )
+                            }
+                        />
+                    ) : (
+                        <button
+                            type="button"
+                            className="relative flex items-center justify-center overflow-hidden text-sm font-medium text-blue_primary_600 rounded-lg group border border-dashed border-gray-400 transition-all ease-in duration-75 w-full hover:border-blue_primary_800 hover:text-blue_primary_800"
+                            onClick={() => setShowCreate(true)}
+                        >
+                            <span className="relative py-4 px-5 flex items-center">
+                                <PlusCircleIcon className="w-4 h-4 mr-1" />
+                                Create new topic
+                            </span>
+                        </button>
+                    )}
+                </div>
+            </section>
+        </div>
+    );
 };
 
 export default NewOneWayInterview;
