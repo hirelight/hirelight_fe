5d9241643bf20c38734b48c4fc0dd8037f928cdf
Nguyen Kien <ngkien299@gmail.com>
2023-09-28 17:33:15 +0700

    Set cookie for subdomain

--- src/middleware.ts
+++ src/middleware.ts
@@ -20,6 +20,7 @@ export default async function middleware(req: NextRequest) {
     // .replace('.localhost:3000', `.${process.env.NEXT_PUBLIC_ROOT_DOMAIN}`);
     // Get the pathname of the request (e.g. /, /about, /blog/first-post)
     const path = url.pathname;
+    const response = NextResponse.next();
     // rewrites for app pages
     // if (hostname == `app.${process.env.NEXT_PUBLIC_ROOT_DOMAIN}`) {
     //   const session = localStorage.getItem('token');
@@ -52,6 +53,13 @@ export default async function middleware(req: NextRequest) {
         hostname === "jobs.localhost:3000" ||
         hostname === `jobs.${process.env.NEXT_PUBLIC_ROOT_DOMAIN}`
     ) {
+        response.cookies.set({
+            name: "hirelight_access_token",
+            value: "eyJhbGciOiJodHRwOi8vd3d3LnczLm9yZy8yMDAxLzA0L3htbGRzaWctbW9yZSNobWFjLXNoYTUxMiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9lbWFpbGFkZHJlc3MiOiJuZ2tpZW4yOTlAZ21haWwuY29tIiwiZW1haWwiOiJuZ2tpZW4yOTlAZ21haWwuY29tIiwiaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93cy8yMDA4LzA2L2lkZW50aXR5L2NsYWltcy9yb2xlIjoiQ0FORElEQVRFIiwicm9sZSI6IkNBTkRJREFURSIsImV4cCI6MTY5NTcyNzIzMX0.q6HjYGDbQc-pKma5yP3XIvXMlie9_M-Yhkgz-F0162YESuGshlyVjSe6I4ONElaE6xM7QYbnfqmsacxrvKzqxg",
+            httpOnly: true,
+            domain: ".locahost:3000",
+        });
+
         return NextResponse.rewrite(new URL(`/interviewee${path}`, req.url));
     }
 
