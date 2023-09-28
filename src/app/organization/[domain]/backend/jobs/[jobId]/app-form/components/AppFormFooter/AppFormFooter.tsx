b2118be89249145a1937caa25cc8b9ae3dd96c49
Nguyen Kien <ngkien299@gmail.com>
2023-09-28 17:44:09 +0700

    Add DeleteModal on confirm delete job

--- src/app/organization/[domain]/backend/jobs/[jobId]/app-form/components/AppFormFooter/AppFormFooter.tsx
+++ src/app/organization/[domain]/backend/jobs/[jobId]/app-form/components/AppFormFooter/AppFormFooter.tsx
@@ -2,28 +2,41 @@
 
 import React from "react";
 
-import { ButtonOutline } from "@/components";
+import { ButtonOutline, DeleteModal, Portal } from "@/components";
 import { useAppDispatch } from "@/redux/reduxHooks";
 import { clearAppForm } from "@/redux/slices/app-form.slice";
+import { SpinLoading } from "@/icons";
 
 const AppFormFooter = () => {
     const dispatch = useAppDispatch();
+    const [showModal, setShowModal] = React.useState(false);
 
-    const handleDeleteJob = () => {
-        dispatch(clearAppForm);
+    const handleShowConfirmModal = () => {
+        setShowModal(true);
+        // dispatch(clearAppForm);
     };
 
     return (
-        <div className="w-full bg-white flex items-center justify-between px-3 py-6 border-t border-gray-300 rounded-bl-lg rounded-br-lg relative z-[2]">
-            <ButtonOutline>Save draft</ButtonOutline>
-            <button
-                type="button"
-                className="text-xs font-medium text-red-600 hover:text-red-800 hover:font-semibold hover:underline"
-                onClick={handleDeleteJob}
-            >
-                Delete job
-            </button>
-        </div>
+        <>
+            <Portal>
+                <DeleteModal
+                    show={showModal}
+                    onClose={() => setShowModal(false)}
+                    onConfirm={() => console.log("Delete job")}
+                />
+            </Portal>
+
+            <div className="w-full bg-white flex items-center justify-between px-3 py-6 border-t border-gray-300 rounded-bl-lg rounded-br-lg relative z-[2]">
+                <ButtonOutline>Save draft</ButtonOutline>
+                <button
+                    type="button"
+                    className="text-xs font-medium text-red-600 hover:text-red-800 hover:font-semibold hover:underline"
+                    onClick={handleShowConfirmModal}
+                >
+                    Delete job
+                </button>
+            </div>
+        </>
     );
 };
 
