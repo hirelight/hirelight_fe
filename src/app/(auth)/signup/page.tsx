506340fc143aca23a9c6ce8b2ecf6b30b8812340
Nguyen Kien <ngkien299@gmail.com>
2023-10-05 21:49:02 +0700

    Implement signup static UI + signup handler

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { FormEvent } from "react";
+import Link from "next/link";
+import { Metadata } from "next";
 
-const SignUp = () => {
-    return <div>SignUp</div>;
+import SignupForm from "./components/SignupForm";
+
+export const metadata: Metadata = {
+    title: "Hirelight Recruitment Software - Sign up",
 };
 
-export default SignUp;
+const Signup = () => {
+    return (
+        <div className="min-w-[500px] min-h-[400px] relative bg-white shadow-lg rounded-md p-8 mx-0 md:mx-6 text-center">
+            <SignupForm />
+            <div className="mt-8 text-sm text-center relative">
+                <span className="text-gray-500 mr-1">
+                    Already have a Hirelight account?
+                </span>
+                <Link
+                    href={"/login"}
+                    className="font-semibold text-blue-600 hover:cursor-pointer hover:underline"
+                >
+                    Sign in
+                </Link>
+            </div>
+        </div>
+    );
+};
+
+export default Signup;
