f6f8d08a1698a1dbd0ba799a343782a430e58dac
Nguyen Kien <ngkien299@gmail.com>
2023-10-05 20:21:46 +0700

    Set cookie for subdomain on login success && Protect route on invalid token

--- src/app/organization/[domain]/page.tsx
+++ src/app/organization/[domain]/page.tsx
@@ -1,16 +1,29 @@
 "use client";
 
 import React from "react";
-import { usePathname, useRouter } from "next/navigation";
+import { redirect } from "next/navigation";
+import Cookies from "js-cookie";
 
-const DomainPage = () => {
-    const router = useRouter();
-    const pathname = usePathname();
-    const hostname = window.location.hostname;
+const DomainPage = ({
+    params,
+    searchParams,
+}: {
+    params: { domain: string };
+    searchParams: { [key: string]: string | string[] | undefined };
+}) => {
+    const { code } = searchParams;
 
     React.useEffect(() => {
-        router.push(`backend`);
-    }, [router]);
+        if (!Cookies.get("hirelight_acess_token"))
+            if (code) {
+                Cookies.set("hirelight_acess_token", code as string);
+            } else {
+                redirect(
+                    `${window.location.protocol}//${process.env.NEXT_PUBLIC_ROOT_DOMAIN}/login`
+                );
+            }
+        redirect(`backend`);
+    }, [code]);
 
     return null;
 };
