37deb765de28a1e520564803f48368aee53e9b7c
Nguyen Kien <ngkien299@gmail.com>
2023-10-08 22:36:40 +0700

    Add page title

--- src/app/organization/[domain]/backend/jobs/[jobId]/(hiring-process-manage)/hiring-process/[assessmentId]/page.tsx
+++ src/app/organization/[domain]/backend/jobs/[jobId]/(hiring-process-manage)/hiring-process/[assessmentId]/page.tsx
@@ -1,5 +1,10 @@
 import React from "react";
+import { Metadata } from "next";
 
 import HiringProcessCandidate from "./candidate/[candidateId]/page";
+
+export const metadata: Metadata = {
+    title: "Hirelight - FPT",
+};
 
 export default HiringProcessCandidate;
