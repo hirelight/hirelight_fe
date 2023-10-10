0c95055e5f6ff31411c2371679a214c3eff65fb4
Nguyen Kien <ngkien299@gmail.com>
2023-10-11 03:45:53 +0700

    Add samesite and secure cookies configuration

--- src/app/organization/[domain]/backend/layout.tsx
+++ src/app/organization/[domain]/backend/layout.tsx
@@ -1,8 +1,21 @@
 import React from "react";
+import { cookies, headers } from "next/headers";
+import { redirect } from "next/navigation";
 
 import ProtectedRoute from "./components/ProtectedRoute";
 
 const BackendLayout = ({ children }: { children: React.ReactNode }) => {
+    const cookieStore = cookies();
+    const token = cookieStore.get("hirelight_access_token");
+    const headerList = headers();
+    const referer = headerList.get("referer");
+
+    if (!token)
+        redirect(
+            `${referer?.split("/")[0]}//www.${
+                process.env.NEXT_PUBLIC_ROOT_DOMAIN
+            }/login`
+        );
     return (
         <div>
             <ProtectedRoute>{children}</ProtectedRoute>
