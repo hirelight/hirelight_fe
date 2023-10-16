5cdfb4b6912c54fdc6b45dd628ab752bb0305dde
Nguyen Kien <ngkien299@gmail.com>
2023-10-17 02:34:39 +0700

    Add breadcrumbs and drawer on lg devices and lower

--- src/app/[lang]/organization/[domain]/backend/settings/layout.tsx
+++ src/app/[lang]/organization/[domain]/backend/settings/layout.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import SettingsSidebar from "./components/SettingsSidebar";
+import HeadNavBar from "./components/HeadNavBar";
 
 const OrganizationSettingsLayout = ({
     children,
@@ -8,11 +9,14 @@ const OrganizationSettingsLayout = ({
     children: React.ReactNode;
 }) => {
     return (
-        <div className="flex-1 flex gap-6 max-w-screen-xl mx-auto p-4 xl:px-6">
-            <div className="w-1/4">
-                <SettingsSidebar />
+        <div>
+            <HeadNavBar />
+            <div className="flex-1 flex gap-6 max-w-screen-xl mx-auto p-4 xl:px-6 pb-12">
+                <div className="w-1/4 hidden lg:block">
+                    <SettingsSidebar />
+                </div>
+                <div className="flex-1">{children}</div>
             </div>
-            <div className="flex-1">{children}</div>
         </div>
     );
 };
