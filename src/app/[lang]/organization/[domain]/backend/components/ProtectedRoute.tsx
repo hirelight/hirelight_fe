dd33c0b8c7b44e51c65c08311b67bbd6616c67f5
Nguyen Kien <ngkien299@gmail.com>
2023-11-25 23:46:48 +0700

    Fix

--- src/app/[lang]/organization/[domain]/backend/components/ProtectedRoute.tsx
+++ src/app/[lang]/organization/[domain]/backend/components/ProtectedRoute.tsx
@@ -8,14 +8,9 @@ import { decryptData } from "@/helpers/authHelpers";
 const ProtectedRoute = ({ children }: { children: React.ReactNode }) => {
     const router = useRouter();
     const token = decryptData("hirelight_access_token");
-    React.useEffect(() => {
-        if (!token && typeof window !== "undefined")
-            router.replace(
-                `${window.location.protocol}//${process.env.NEXT_PUBLIC_ROOT_DOMAIN}/login`
-            );
-    }, [router, token]);
+    console.log(token);
 
-    return !decryptData("hirelight_access_token") ? null : <>{children}</>;
+    return <>{children}</>;
 };
 
 export default ProtectedRoute;
