5dc059da8de83ce7c08a38b7d8c32dfe4dbe19ec
Nguyen Kien <ngkien299@gmail.com>
2023-12-16 23:59:29 +0700

    Add activity loading

--- src/app/[lang]/organization/[domain]/backend/jobs/[jobId]/(hiring-process-manage)/(statistics)/activities/components/ActivityList.tsx
+++ src/app/[lang]/organization/[domain]/backend/jobs/[jobId]/(hiring-process-manage)/(statistics)/activities/components/ActivityList.tsx
@@ -10,10 +10,12 @@ import ActivityCard from "./ActivityCard";
 
 const ActivityList = () => {
     const { jobId } = useParams();
-    const { data: activityRes } = useQuery({
+    const { data: activityRes, isLoading } = useQuery({
         queryKey: ["activities", jobId],
         queryFn: () => activityServices.getListByJobId(jobId as string),
     });
+
+    if (isLoading) return <ActivityListSkeleton />;
 
     return (
         <ul>
@@ -30,3 +32,29 @@ const ActivityList = () => {
 };
 
 export default ActivityList;
+
+const ActivityListSkeleton = () => {
+    return (
+        <ul>
+            {new Array(6).fill("").map((_, index) => (
+                <li
+                    key={index}
+                    className="py-4 first:pt-0 border-b border-gray-300 last:border-b-0"
+                >
+                    <div className="flex animate-pulse">
+                        <div className="w-2/3">
+                            <div className="w-80 h-5 rounded bg-slate"></div>
+                        </div>
+                        <div className="w-1/3 pl-2 flex items-center">
+                            <div className="w-8 h-8 rounded-full bg-slate-300"></div>
+                            <div>
+                                <p className="h-5 w-32 rounded bg-slate-200 mb-2"></p>
+                                <p className="w-40 h-5 rounded bg-slate-200"></p>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+            ))}
+        </ul>
+    );
+};
